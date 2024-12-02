3007ca09ea116c604aedde03bb43f12326282419
DanielSnadon <danielsnadon@gmail.com>
2024-12-02 21:48:23 +0300

    0.01b
    - Добавил возможность заходить на страницу товара и обратно с помощью нажатия на место, где должно быть лого.
    - Определил основную проблему с импортом изображений и css файлов.
    - Разработал новый вариант импорта на случай если баг исправить не удастся.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-# main.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/index.html")
+def back():
+    return render_template("index.html")
+
+
+@app.route("/single.html")
+def item():
+    return render_template("single.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
